b5ffb11f39c6796ea203cd160bcf8d4e5e0b7750
achizhikov <chizhikov.alexey@icloud.com>
2021-11-30 14:32:39 +0300

    fix rounding to short report

--- src/main/kotlin/com/achizhikov/kotlinutilitiesbot/reports/Reports.kt
+++ src/main/kotlin/com/achizhikov/kotlinutilitiesbot/reports/Reports.kt
@@ -3,6 +3,7 @@ package com.achizhikov.kotlinutilitiesbot.reports
 import com.achizhikov.kotlinutilitiesbot.database.Tariff
 import com.achizhikov.kotlinutilitiesbot.database.UtilitiesData
 import com.achizhikov.kotlinutilitiesbot.reports.text.UtilitiesConsumption
+import java.text.DecimalFormat
 import java.time.LocalDate
 import kotlin.math.roundToInt
 
@@ -18,11 +19,11 @@ data class ShortReport(
     )
 
     fun toUiString(): String {
-        val electricityCost = (consumption.electricity * tariff.electricity!!).times(100).roundToInt().div(100)
-        val hotWaterCost = (consumption.hotWater * tariff.hotWater!!).times(100).roundToInt().div(100)
-        val coldWaterCost = (consumption.coldWater * tariff.coldWater!!).times(100).roundToInt().div(100)
-        val drainageCost = (consumption.drainage * tariff.drainage!!).times(100).roundToInt().div(100)
-        val finalCost = electricityCost + hotWaterCost + coldWaterCost + drainageCost
+        val electricityCost = (consumption.electricity * tariff.electricity!!).times(100).roundToInt().div(100.0)
+        val hotWaterCost = (consumption.hotWater * tariff.hotWater!!).times(100).roundToInt().div(100.0)
+        val coldWaterCost = (consumption.coldWater * tariff.coldWater!!).times(100).roundToInt().div(100.0)
+        val drainageCost = (consumption.drainage * tariff.drainage!!).times(100).roundToInt().div(100.0)
+        val finalCost = (electricityCost + hotWaterCost + coldWaterCost + drainageCost).times(100).roundToInt().div(100.0)
         return """
         Короткий отчёт от $date:
         электроэнергия: ${consumption.electricity} x ${tariff.electricity} = $electricityCost₽
