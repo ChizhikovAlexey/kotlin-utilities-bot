f47933c20d3d010499d0e7794895b942b4920e93
achizhikov <chizhikov.alexey@icloud.com>
2021-12-05 23:39:31 +0300

    refactor excel generation

--- src/main/kotlin/com/achizhikov/kotlinutilitiesbot/reports/excel/ExcelService.kt
+++ src/main/kotlin/com/achizhikov/kotlinutilitiesbot/reports/excel/ExcelService.kt
@@ -1,6 +1,8 @@
 package com.achizhikov.kotlinutilitiesbot.reports.excel
 
 import com.achizhikov.kotlinutilitiesbot.reports.Report
+import org.apache.poi.xssf.usermodel.XSSFRow
+import org.apache.poi.xssf.usermodel.XSSFSheet
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.File
 import java.io.FileOutputStream
@@ -25,72 +27,39 @@ fun createExcel(reports: List<Report>): File? {
                 prevData.kitchenHotWater - prevData.kitchenColdWater
 
 
-        val firstRow = sheet.createRow(rowNumber)
+        val firstRow = sheet.createRow(rowNumber++)
         firstRow.createCell(1).setCellValue("новые показания")
         firstRow.createCell(2).setCellValue("предыдущие показания")
         firstRow.createCell(3).setCellValue("расход")
         firstRow.createCell(4).setCellValue("тариф")
         firstRow.createCell(5).setCellValue("стоимость")
         firstRow.createCell(6).setCellValue("дата")
-        rowNumber++
-        rowNumberForFormulas++
 
-        val secondRow = sheet.createRow(rowNumber)
-        secondRow.createCell(0).setCellValue("электроэнергия")
-        secondRow.createCell(1).setCellValue(newData.electricity.toDouble())
-        secondRow.createCell(2).setCellValue(prevData.electricity.toDouble())
-        secondRow.createCell(3).cellFormula = "B${rowNumberForFormulas}-C${rowNumberForFormulas}"
-        secondRow.createCell(4).setCellValue(tariff.electricity.toDouble())
-        secondRow.createCell(5).cellFormula = "D${rowNumberForFormulas}*E${rowNumberForFormulas}"
+        val secondRow = sheet.fillDataRow(
+            rowNumber++, "электроэнергия", newData.electricity, prevData.electricity, tariff.electricity
+        )
         secondRow.createCell(6).setCellValue(newData.date)
         secondRow.getCell(6).cellStyle = dateCellStyle
-        rowNumber++
-        rowNumberForFormulas++
-
-        val thirdRow = sheet.createRow(rowNumber)
-        thirdRow.createCell(0).setCellValue("[ванная] горячая вода")
-        thirdRow.createCell(1).setCellValue(newData.bathHotWater.toDouble())
-        thirdRow.createCell(2).setCellValue(prevData.bathHotWater.toDouble())
-        thirdRow.createCell(3).cellFormula = "B${rowNumberForFormulas}-C${rowNumberForFormulas}"
-        thirdRow.createCell(4).setCellValue(tariff.hotWater.toDouble())
-        thirdRow.createCell(5).cellFormula = "D${rowNumberForFormulas}*E${rowNumberForFormulas}"
-        rowNumber++
-        rowNumberForFormulas++
-
-        val fourthRow = sheet.createRow(rowNumber)
-        fourthRow.createCell(0).setCellValue("[ванная] холодная вода")
-        fourthRow.createCell(1).setCellValue(newData.bathColdWater.toDouble())
-        fourthRow.createCell(2).setCellValue(prevData.bathColdWater.toDouble())
-        fourthRow.createCell(3).cellFormula = "B${rowNumberForFormulas}-C${rowNumberForFormulas}"
-        fourthRow.createCell(4).setCellValue(tariff.coldWater.toDouble())
-        fourthRow.createCell(5).cellFormula = "D${rowNumberForFormulas}*E${rowNumberForFormulas}"
-        rowNumber++
-        rowNumberForFormulas++
-
-        val fifthRow = sheet.createRow(rowNumber)
-        fifthRow.createCell(0).setCellValue("[кухня] горячая вода")
-        fifthRow.createCell(1).setCellValue(newData.kitchenHotWater.toDouble())
-        fifthRow.createCell(2).setCellValue(prevData.kitchenHotWater.toDouble())
-        fifthRow.createCell(3).cellFormula = "B${rowNumberForFormulas}-C${rowNumberForFormulas}"
-        fifthRow.createCell(4).setCellValue(tariff.hotWater.toDouble())
-        fifthRow.createCell(5).cellFormula = "D${rowNumberForFormulas}*E${rowNumberForFormulas}"
-        rowNumber++
-        rowNumberForFormulas++
-
-        val sixthRow = sheet.createRow(rowNumber)
-        sixthRow.createCell(0).setCellValue("[кухня] холодная вода")
-        sixthRow.createCell(1).setCellValue(newData.kitchenColdWater.toDouble())
-        sixthRow.createCell(2).setCellValue(prevData.kitchenColdWater.toDouble())
-        sixthRow.createCell(3).cellFormula = "B${rowNumberForFormulas}-C${rowNumberForFormulas}"
-        sixthRow.createCell(4).setCellValue(tariff.coldWater.toDouble())
-        sixthRow.createCell(5).cellFormula = "D${rowNumberForFormulas}*E${rowNumberForFormulas}"
-        rowNumber++
-        rowNumberForFormulas++
+        sheet.fillDataRow(
+            rowNumber++, "электроэнергия", newData.electricity, prevData.electricity, tariff.electricity
+        )
+        sheet.fillDataRow(
+            rowNumber++, "[ванная] горячая вода", newData.bathHotWater, prevData.bathHotWater, tariff.hotWater
+        )
+        sheet.fillDataRow(
+            rowNumber++, "[ванная] холодная вода", newData.bathColdWater, prevData.bathColdWater, tariff.coldWater
+        )
+        sheet.fillDataRow(
+            rowNumber++, "[кухня] горячая вода", newData.kitchenHotWater, prevData.kitchenHotWater, tariff.hotWater
+        )
+        sheet.fillDataRow(
+            rowNumber++, "[кухня] холодная вода", newData.kitchenHotWater, prevData.kitchenHotWater, tariff.hotWater
+        )
 
         val seventhRow = sheet.createRow(rowNumber)
         seventhRow.createCell(0).setCellValue("водоотведение")
         seventhRow.createCell(3).setCellValue(drainage.toDouble())
-        seventhRow.createCell(4).setCellValue(tariff.drainage.toDouble())
+        seventhRow.createCell(4).setCellValue(tariff.drainage)
         seventhRow.createCell(5).cellFormula = "D${rowNumberForFormulas}*E${rowNumberForFormulas}"
         rowNumber++
         rowNumberForFormulas++
@@ -115,6 +84,23 @@ fun createExcel(reports: List<Report>): File? {
     } catch (exc: Exception) {
         null
     }
+}
+
+private fun XSSFSheet.fillDataRow(
+    rowNumber: Int,
+    type: String,
+    value: Int,
+    previousValue: Int,
+    tariff: Double
+): XSSFRow {
+    val row = this.createRow(rowNumber)
+    row.createCell(0).setCellValue(type)
+    row.createCell(1).setCellValue(value.toDouble())
+    row.createCell(2).setCellValue(previousValue.toDouble())
+    row.createCell(3).cellFormula = "B${rowNumber + 1}-C${rowNumber + 1}"
+    row.createCell(4).setCellValue(tariff)
+    row.createCell(5).cellFormula = "D${rowNumber + 1}*E${rowNumber + 1}"
+    return row
 }
 
 
